370421e6a6aae1068bc8df344a1f7d59f36207a1
Andrey Khomutov <homutovan@live.com>
2020-09-02 00:38:09 +0700

    fix eslint errors

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
 export default function phoneCleaner(dryPhone) {
   return dryPhone.match(/(\+?\d*)/g).filter((e) => e && e)
-    .map((e, i) => !i && e[0] != '+' ? e = e.replace(/(7|8)/, '+7') : e).join('');
+    .map((e, i) => !i && e[0] !== '+' ? e = e.replace(/(7|8)/, '+7') : e).join('');
 }
